3c883e4a6dfe3bd80af1045cee414529ec346517
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2020-03-07 19:06:51 -0300

    Add location to dev

--- api/src/models/Dev.js
+++ api/src/models/Dev.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose';
+import PointSchema from './utils/PointSchema';
 
 const DevSchema = new mongoose.Schema({
   name: String,
-  github_username: String,
+  githubUsername: String,
   bio: String,
-  avatar_url: String,
-  techs: [String]
+  avatarUrl: String,
+  techs: [String],
+  location: {
+    type: PointSchema,
+    index: '2dsphere'
+  }
 });
 
 export default mongoose.model('dev', DevSchema);
