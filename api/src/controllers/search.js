c3a5008eeed4b307770755eb86e9c2663925ec15
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2020-03-08 16:02:41 -0300

    Add try catch to search controller

--- api/src/controllers/search.js
+++ api/src/controllers/search.js
@@ -5,21 +5,23 @@ exports.search = async (req, res) => {
   const { techs, latitude, longitude } = req.query;
   const techArray = parseStringAsArray(techs);
 
-  const devs = await devService.findDevs({
-    techs: {
-      $in: techArray
-    },
-    location: {
-      $near: {
-        $geometry: {
-          type: 'Point',
-          coordinates: [longitude, latitude]
-        },
-        $maxDistance: 10000
+  try {
+    const devs = await devService.findDevs({
+      techs: {
+        $in: techArray
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000
+        }
       }
-    }
-  });
-
-  return res.json({ devs });
-  return res.status(400).json({ message: 'Server error' });
+    });
+    return res.json({ devs });
+  } catch (err) {
+    return res.status(400).json({ message: 'Server error' });
+  }
 };
